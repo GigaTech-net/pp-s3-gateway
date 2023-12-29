2d0febe45c21126cc3891da16868cb063faeed53
Matt Jenks <mattjenks@gmail.com>
2023-12-29 10:56:13 -0500

    PRO-440 add S3 variables to terraform ecs

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -3,18 +3,28 @@ resource "template_dir" "task_definition" {
   destination_dir = "${path.module}/rendered"
 
   vars = {
-    execution_role_arn       = var.execution_role_arn
-    task_role_arn            = var.task_role_arn
-    region                   = var.region
-    ecs_service_name         = var.ecs_service_name
-    task_definition_name     = var.task_definition_name
-    family                   = var.family
-    container_name           = var.container_name
-    env                      = var.env
-    docker_image_url         = ""
-    cpu                      = 512
-    memory                   = 1024
-    docker_container_port    = 8080
+    execution_role_arn                  = var.execution_role_arn
+    task_role_arn                       = var.task_role_arn
+    region                              = var.region
+    ecs_service_name                    = var.ecs_service_name
+    task_definition_name                = var.task_definition_name
+    family                              = var.family
+    container_name                      = var.container_name
+    env                                 = var.env
+    s3_server                           = var.s3_server
+    s3_server_port                      = var.s3_server_port
+    s3_server_proto                     = var.s3_server_proto
+    s3_style                            = var.s3_style
+    debug                               = var.debug
+    aws_sigs_version                    = var.aws_sigs_version
+    allow_directory_list                = var.allow_directory_list
+    provide_index_page                  = var.provide_index_page
+    append_slash_for_possible_directory = var.append_slash_for_possible_directory
+    directory_listing_path_prefix       = var.directory_listing_path_prefix
+    docker_image_url                    = ""
+    cpu                                 = 512
+    memory                              = 1024
+    docker_container_port               = 8080
   }
 }
 
